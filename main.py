8fbfb2a80b5dc957169002d2aa7cf4691befa31c
varsha-1428 <mandadisrivarsha28@gmail.com>
2026-06-01 00:35:10 +0530

    add workflow state

--- main.py
+++ main.py
@@ -7,6 +7,10 @@ from langchain_core.messages import HumanMessage, SystemMessage
 from typing import TypedDict
 from models import PostAnalysis
 
+class GraphState(TypedDict):
+    post: str
+    analysis: PostAnalysis
+
 llm = ChatGroq(model="qwen/qwen3-32b")
 structured_llm = llm.with_structured_output(PostAnalysis)
 
@@ -23,10 +27,4 @@ response = structured_llm.invoke([
     ),
     HumanMessage(content="LangGraph feels very difficult to learn for beginners")
 ])
-print(response) # can also access result.category, result.summary, result.toxic
-# try with HumanMessage(content="I hate everyone in this community")
-
-if response.toxic:
-    print("send for moderator review")
-else:
-    print("publish")
+print(response)
