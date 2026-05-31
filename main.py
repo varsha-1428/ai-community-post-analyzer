d30f3532e1b4b8ad6d600053dc41379d070cd75d
varsha-1428 <mandadisrivarsha28@gmail.com>
2026-06-01 01:11:12 +0530

    create basic langGraph workflow

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ load_dotenv()
 
 from langchain_groq import ChatGroq
 from langchain_core.messages import HumanMessage, SystemMessage
+from langgraph.graph import StateGraph, START, END
 from typing import TypedDict
 from models import PostAnalysis
 
@@ -20,10 +21,14 @@ def analyze_post(state:GraphState):
         "analysis": result
     }
 
-state = {
+builder = StateGraph(GraphState)
+builder.add_node("analyze_post", analyze_post)
+builder.add_edge(START, "analyze_post")
+builder.add_edge("analyze_post", END)
+graph = builder.compile()
+
+result = graph.invoke({
     "post": "LangGraph feels difficult for beginners."
-}
+})
 
-result = analyze_post(state)
-
-print(result)
+print(result) # analysis also gets added
