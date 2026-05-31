5470284bd370ef2470d83c51dfa9e1a963bfb7f5
varsha-1428 <mandadisrivarsha28@gmail.com>
2026-05-31 23:34:56 +0530

    system & user messages

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ from dotenv import load_dotenv
 load_dotenv()
 
 from langchain_groq import ChatGroq
+from langchain_core.messages import HumanMessage, SystemMessage
 llm = ChatGroq(model="qwen/qwen3-32b")
-response = llm.invoke("Hi, How are you?")
+response = llm.invoke([
+    SystemMessage(content="Reply in exactly 3 words"),
+    HumanMessage(content="I am learning langgraph.")
+])
 print(response.content)
